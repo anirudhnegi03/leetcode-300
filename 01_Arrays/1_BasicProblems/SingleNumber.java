fb68082c89c38fac6cb7b5de66dfefb7d622e688
Anirudh Negi <148575136+anirudhnegi03@users.noreply.github.com>
2025-07-17 09:15:49 +0530

    Create SingleNumber.java

---
+++ 01_Arrays/1_BasicProblems/SingleNumber.java
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/single-number/
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans=0;
+        for(int num:nums){
+            ans=ans^num;
+        }
+        return ans;
+    }
+}
